cd279461dbc0543b1a5c66407caac77c8525ab99
Sergi <sergi.prof@hotmail.com>
2025-01-22 12:53:33 +0100

    feat: add active application-postgres.properties and creates tables in postgres db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
 
 /* Users */
-INSERT INTO users (username, password) VALUES ('pepe', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
-INSERT INTO users (username, password) VALUES ('pepa', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
+INSERT INTO users (username, password) VALUES ('Mohammed', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
+INSERT INTO users (username, password) VALUES ('Abdeljawel', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
 
 /* Roles Users */
 INSERT INTO roles_users (role_id, user_id) VALUES (1, 1);
